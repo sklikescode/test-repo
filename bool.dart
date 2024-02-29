d6a1fb58e14ea7b615a03d2be58b3e7e40fe9eca
sklikescode <karikonyistephen@gmail.com>
2024-02-29 14:42:05 +0300

    more changes on variables

---
+++ bool.dart
@@ -0,0 +1,25 @@
+void main() {
+  //Declaring variables
+  String schoolName = 'Powerlearn Project Academy';
+  String address = 'Africa';
+  String module = 'Dart';
+  int num1 = 5;
+  double num2 = 10.2;
+  bool isMarried = true;
+
+  //lists
+  var cars = ["BMW", "Audi", "VW", "Benz"];
+
+  //Maps
+  Map age = {'Kamash': 14, 'Effie': 21, 'Tom': 16};
+
+
+  print(schoolName);
+  print(address);
+  print(module);
+  print(num1);
+  print(num2);
+  print("marriage status:$isMarried");
+  print(cars[0]);
+  print(age);
+}
